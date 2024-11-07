59cddebb4bf6d3d224750fbc1bffbe547d47a976
Rosario Di Chiara <dchross97@outlook.it>
2024-11-07 19:00:19 +0100

    Selfwork corretto

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 
 
 function contaCifre (numero) {
-    if (numero > 0 && numero < 10) {
-        console.log(`${contaCifre(9)} 1 cifra`);  
+    if (numero < 10 ) {
+        return '1 cifra'  
+    }else if (numero < 100){
+        return '2 cifre';
         
-        
-        
-        
+    }else if (numero < 1000) {
+        return '3 cifre'
+    }else if (numero < 10000) {
+        return '4 cifre'
     }else{
-        return 'Numero troppo grande';
-        
+        return 'numero troppo grande'
     }
     
 }
 
+console.log(contaCifre(109));

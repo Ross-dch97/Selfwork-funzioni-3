75fd6407905e1991396c9125b55882729a0357f7
Rosario Di Chiara <dchross97@outlook.it>
2024-11-07 18:46:50 +0100

    Selfwork funzioni 3

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function contaCifre (numero) {
+    if (numero > 0 && numero < 10) {
+        console.log(`${contaCifre(9)} 1 cifra`);  
+        
+        
+        
+        
+    }else{
+        return 'Numero troppo grande';
+        
+    }
+    
+}
+
